141b14271a7713950ae5e8c29a99e2d62d6ca6d2
Igor Oliveira <igor27oliver@gmail.com>
2026-07-23 07:31:28 -0300

    Remove pasta target do controle de versão

--- target/classes/Db/V1_create_table_banda.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE banda (
-    id            BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name          VARCHAR(150) NOT NULL,
-    country       VARCHAR(100),
-    description   VARCHAR(2000),
-    year_start    INT
-);
